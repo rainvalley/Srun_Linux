7f72ce7dcbf084172a9e31914261eb9c28b39e9e
CHxCOOH <NaHCOx@qq.com>
2022-09-14 15:32:38 +0000

    [更新]自动登录：现在仅在下线时发送登录请求

--- srun.sh
+++ srun.sh
@@ -1,40 +1,54 @@
 #!/bin/ash
-Stu_No=$1
-Stu_Passwd=$2
-URL="http://172.16.154.130:69/cgi-bin/srun_portal"
-Encrypted_No="{SRUN3}\r\n"
-Encrypted_Passwd=""
+signin() {
+    Stu_No=$1
+    Stu_Passwd=$2
+    URL="http://172.16.154.130:69/cgi-bin/srun_portal"
+    Encrypted_No="{SRUN3}\r\n"
+    Encrypted_Passwd=""
 
-for i in `seq ${#Stu_No}`
-do
-    letter=$(printf "%d" "'${Stu_No:$(($i-1)):1}")
-    let letter=letter+4
-    letter=$(printf \\x`printf %x $letter`)
-    Encrypted_No=$Encrypted_No$letter
-done
+    for i in `seq ${#Stu_No}`
+    do
+        letter=$(printf "%d" "'${Stu_No:$(($i-1)):1}")
+        let letter=letter+4
+        letter=$(printf \\x`printf %x $letter`)
+        Encrypted_No=$Encrypted_No$letter
+    done
 
-for i in `seq ${#Stu_Passwd}`
-do
-    i=$(($i-1))
-    letter=$(printf "%d" "'${Stu_Passwd:$i:1}")
-    if test $i -eq 0
+    for i in `seq ${#Stu_Passwd}`
+    do
+        i=$(($i-1))
+        letter=$(printf "%d" "'${Stu_Passwd:$i:1}")
+        if test $i -eq 0
+        then
+            ki=$(($letter^48))
+        else
+            ki=$(($letter^((10-i%10)+48)))
+        fi
+        _l=$((($ki&0x0f)+0x36))
+        _h=$((($ki>>4&0x0f)+0x63))
+        _l=$(printf \\x`printf %x $_l`)
+        _h=$(printf \\x`printf %x $_h`)
+        if  test $(($i%2)) -eq 1
+        then
+            result=$_h$_l
+        else
+            result=$_l$_h
+        fi
+        Encrypted_Passwd=$Encrypted_Passwd$result
+    done
+
+    #echo `urlencode $Encrypted_No`
+    #echo `urlencode $Encrypted_Passwd`
+    sigin_result=$(wget -qO- --post-data=$(printf "username=";urlencode $Encrypted_No;printf "&password=";urlencode $Encrypted_Passwd;printf "&ac_id=1&action=login&type=3&n=117&mbytes=0&minutes=0&drop=0&pop=1&mac=02:00:00:00:00:00") $URL
+)
+    echo $sigin_result
+    if [[ $sigin_result == login_ok* ]]
     then
-        ki=$(($letter^48))
+        return 1
     else
-        ki=$(($letter^((10-i%10)+48)))
+        return 0
     fi
-    _l=$((($ki&0x0f)+0x36))
-    _h=$((($ki>>4&0x0f)+0x63))
-    _l=$(printf \\x`printf %x $_l`)
-    _h=$(printf \\x`printf %x $_h`)
-    if  test $(($i%2)) -eq 1
-    then
-        result=$_h$_l
-    else
-        result=$_l$_h
-    fi
-    Encrypted_Passwd=$Encrypted_Passwd$result
-done
+}
 
 urlencode() {
     # urlencode <string>
@@ -49,6 +63,20 @@ urlencode() {
     done
 }
 
-#echo `urlencode $Encrypted_No`
-#echo `urlencode $Encrypted_Passwd`
-wget -qO- --post-data=$(printf "username=";urlencode $Encrypted_No;printf "&password=";urlencode $Encrypted_Passwd;printf "&ac_id=1&action=login&type=3&n=117&mbytes=0&minutes=0&drop=0&pop=1&mac=02:00:00:00:00:00") $URL
+# 220911开启了防共享检测，这里模拟设备UA，可自行修改（或删除）--user-agent参数
+test_result=$(wget -qO- http://connect.rom.miui.com/generate_204 --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33")
+if [ -z "$test_result" ]; then
+    echo "User is online"
+else
+    echo "User is offline, trying to reconnect..."
+    echo "$(date '+%Y-%m-%d %X') User is offline, trying to login..." >> ~/login.log
+    # $1 用户名 $2 密码
+    signin $1 $2
+    if test $? -eq 1; then
+        echo "User is connected"
+        echo "$(date '+%Y-%m-%d %X') User is logined successfully" >> ~/login.log
+    else
+        echo "User connect failed"
+        echo "$(date '+%Y-%m-%d %X') User failed to login" >> ~/login.log
+    fi
+fi
